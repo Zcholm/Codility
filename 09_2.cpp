b0e7b31761c19248ea5c5263ee27468b6266e838
Simon Holm <simon@holm.nu>
2019-11-22 13:45:24 +0100

    updated

--- 09_2.cpp
+++ 09_2.cpp
@@ -1,9 +1,9 @@
 int solution(vector<int> &A) {
     int N = A.size();
-    int maxEnding = 0;
-    int maxSlice = 0;
-    for (int i = 0; i < N; i++) {
-        maxEnding = max(0, maxEnding+A[i]);
+    int maxEnding = A[0];
+    int maxSlice = A[0];
+    for (int i = 1; i < N; i++) {
+        maxEnding = max(A[i], maxEnding+A[i]);
         maxSlice = max(maxSlice, maxEnding);
     }
     return maxSlice;
